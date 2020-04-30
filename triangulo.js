a0851129a96ceac897fb6c02236109ce0f52957c
sonia <ali.caceres.so@gmail.com>
2020-04-29 21:46:50 -0400

    Fix delete text error

--- triangulo.js
+++ triangulo.js
@@ -6,38 +6,38 @@ function calcular() {
 
     var num3 = document.getElementById("num3").value;
 
-    if (num1>0 && num2 > 0 && num3>0 ) {
-        
-    // calcular resultado
-    var res = ""
-    if (num1 == num2 && num1 != num3 || num2 == num3 && num3 != num1 || num3 == num1 && num1 != num2) {
-        res = "Isosceles";
+    if (num1 > 0 && num2 > 0 && num3 > 0) {
 
-    } else if (num1 == num3 && num3 == num2) {
-        res = "Equilatero";
-        
+        // calcular resultado
+        var res = ""
+        if (num1 == num2 && num1 != num3 || num2 == num3 && num3 != num1 || num3 == num1 && num1 != num2) {
+            res = "Isosceles";
 
-    } else {
-        res = "Escaleno";
-        
-    }
-    // mostrar el resultado
-    document.getElementById("resultado").innerHTML = "El triangulo es: " + res;
-    
+        } else if (num1 == num3 && num3 == num2) {
+            res = "Equilatero";
+
+
+        } else {
+            res = "Escaleno";
+
+        }
+        // mostrar el resultado
+        document.getElementById("resultado").innerHTML = "El triangulo es: " + res;
+        // borrar los datos
+        document.getElementById("num1").value = "";
+        document.getElementById("num2").value = "";
+        document.getElementById("num3").value = "";
+
     } else {
         alert("Introduzca un numero positivo y mayor a 'cero' ")
     }
-    // borrar los datos
-    document.getElementById("num1").value = "";
-    document.getElementById("num2").value = "";
-    document.getElementById("num3").value = "";
-    
+
 }
 
 
 function verificar(e) {
-   var num = e.target.value
-    if(num <= 0){
+    var num = e.target.value
+    if (num <= 0) {
         alert("introduzca un numero positivo mayor a cero")
         e.target.value = ""
         e.target.focus()
